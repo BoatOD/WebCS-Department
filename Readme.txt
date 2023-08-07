98dd0a3e5fe13cc80fca6314b87e1fbe8007ceed
kitsadi <kitsadi_than@cmu.ac.th>
2023-08-07 21:36:03 +0700

    Connect db.

--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,4 @@
+[optional] docker system prune -a
+
 chmod +x run_docker_compose.sh
 ./run_docker_compose.sh
--- server/server.js
+++ server/server.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { Client } = require('pg');
+
 const PORT = 8080;
 
 app.use(cors());
 
-app.get("/api/home", (req, res) => {
-    res.json({ message: "Like this video!", people: ["Arpan", "Jack", "Barry"] });
-})
+const client = new Client({
+    host: "db",
+    user: "postgres",
+    port: "5432",
+    password: "root",
+    database: "csproject"
+});
+
+client.connect();
+
+app.get('/db', async (req, res) => {
+    try {
+        // const client = await pool.connect();
+        const result = await client.query('SELECT 1');
+        return res.send('<h1>db works.</h1>');
+    } catch (err) {
+        return res.send('<h1>db is broken.</h1>' + err.toString());
+    }
+});
+
+app.get('/api/home', async (req, res) => {
+    try {
+        res.json({ message: 'Like this video!', people: ['Arpan', 'Jack', 'Barry'] });
+    } catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.get('/', async (req, res) => {
+    try {
+        res.send('Backend works.');
+    } catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
