0030c8b35753479d758f5951063db0a72372ce27
KEN <thn_ken@DESKTOP-696UU0R.localdomain>
2023-09-10 23:09:02 +0700

    update-first-page-web

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-  }
-  
-  module.exports = {
-    webpackDevMiddleware: (config) => {
-      // Solve compiling problem via vagrant
-      config.watchOptions = {
-        poll: 1000,   // Check for changes every second
-        aggregateTimeout: 300,   // delay before rebuilding
-      };
-      return config;
-    }
-  };
+const nextConfig = {}
+
+module.exports = nextConfig
