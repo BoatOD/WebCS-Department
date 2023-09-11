2443f9fc9167f92b9da857a18e50ec26fc9902e2
kitsadi <kitsadi_than@cmu.ac.th>
2023-09-11 14:31:05 +0700

    edit some config file

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    webpackDevMiddleware: (config) => {
+      // Solve compiling problem via vagrant
+      config.watchOptions = {
+        poll: 1000,   // Check for changes every second
+        aggregateTimeout: 300,   // delay before rebuilding
+      };
+      return config;
+    }
+  };
