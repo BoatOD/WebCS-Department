f52e510ab147aa21cb6985a3c09e4846fb5f8594
kitsadi <kitsadi_than@cmu.ac.th>
2023-09-21 19:53:42 +0700

    update sidebar hover on page undergraduate

--- client/app/(home)/academics/undergraduate/page.tsx
+++ client/app/(home)/academics/undergraduate/page.tsx
@@ -1,18 +1,29 @@
 "use client"
 import React from "react";
-import Undergraduate from "@/components/AcademicPage/Undergraduate";
+import Banner from '@/components/AcademicPage/Banner';
 import { Sidebar } from "flowbite-react";
 import Image from "next/image";
 import { SidebarCustomTheme } from "@/theme";
 
 type Props = {};
 
-export default function page({}: Props) {
+export default function undergraduate({}: Props) {
   return (
     <>
+    <Banner
+        imageSrc="/academic_banner.png" // Adjust the image path for this page
+        altText="Image Alt Text"
+        title="ACADEMIC"
+        subtitle="UNDERGRADUATE"
+        customStyles={{
+          width: '500px',  // Custom width for this page
+          height: '100px',  // Custom height for this page
+          bottom: '-15px', // Custom bottom attribute for this page
+        }}
+      />
       <div className="flex flex-col md:flex-row  mx-auto w-full max-w-screen-xl px-[1rem]  gap-[3rem]">
         <div className="w-full md:w-2/3 order-last md:order-first ">
-          <div className="flex flex-col space-y-2 mt-7">
+          <div className="flex flex-col space-y-2 mt-20">
             <div className="px-8 py-0.5 bg-black w-full "></div>
           </div>
           <div className="p-1 pt-3 ">
@@ -42,67 +53,57 @@ export default function page({}: Props) {
           </div>
         </div>
         <div className="w-full md:w-1/3 order-first md:order-last ">
-          <div className="flex flex-col space-y-2 mt-7">
+          <div className="flex flex-col space-y-2 mt-20">
             <div className="px-8 py-0.5 bg-black w-full"></div>
             <Sidebar theme={SidebarCustomTheme} className="w-full">
-              <Sidebar.Items className="bg-white p-0">
-                <Sidebar.ItemGroup className="bg-white p-0">
-                  <Sidebar.Item href="#">
+              <Sidebar.Items className="p-0">
+                <Sidebar.ItemGroup className="p-0">
+                  <Sidebar.Item className="text-lg font-bold hover:bg-transparent">
                     <p>UNDERGRADUATE</p>
                   </Sidebar.Item>
-                  <div className="flex flex-col space-y-1 mt-7">
-                    <div className="px-8 py-0.5 bg-slate-500"></div>
-                  </div>
-                  <Sidebar.Item href="#">
+                  <div className="border-b border-black border-3 my-2"></div>
+                  <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">
                     <p>Courses</p>
                   </Sidebar.Item>
-                  <div className="flex flex-col space-y-1 mt-7">
-                    <div className="px-8 py-0.5 bg-slate-500"></div>
-                  </div>
-                  <Sidebar.Collapse label="Undergraduate 64">
-                    <Sidebar.Item href="#">Program</Sidebar.Item>
-                    <Sidebar.Item href="#">required courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Core courses</Sidebar.Item>
-                    <Sidebar.Item href="#">GE electric courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Free electric courses </Sidebar.Item>
-                    <Sidebar.Item href="#">
+                  <div className="border-b border-black my-2"></div>
+                  <Sidebar.Collapse className="hover:bg-[#e8e8e8] " label="Undergraduate 64">
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Program</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">required courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Core courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">GE electric courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Free electric courses </Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">
                       Plan-specific Compulsory Courses
                     </Sidebar.Item>
-                    <Sidebar.Item href="#">Compulsory Courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Major Elective Courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Minor Courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Compulsory Courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Major Elective Courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Minor Courses</Sidebar.Item>
                   </Sidebar.Collapse>
-                  <div className="flex flex-col space-y-1 mt-7">
-                    <div className="px-8 py-0.5 bg-slate-500"></div>
-                  </div>
+                  <div className="border-b border-black my-2"></div>
 
-                  <Sidebar.Collapse label="undergraduate 59 (63)">
-                    <Sidebar.Item href="#">Program</Sidebar.Item>
-                    <Sidebar.Item href="#">required courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Core courses</Sidebar.Item>
-                    <Sidebar.Item href="#">GE electric courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Free electric courses </Sidebar.Item>
-                    <Sidebar.Item href="#">
+                  <Sidebar.Collapse className="hover:bg-[#e8e8e8] " label="undergraduate 59 (63)">
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Program</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">required courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Core courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">GE electric courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Free electric courses </Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">
                       Plan-specific Compulsory Courses
                     </Sidebar.Item>
-                    <Sidebar.Item href="#">Compulsory Courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Major Elective Courses</Sidebar.Item>
-                    <Sidebar.Item href="#">Minor Courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Compulsory Courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Major Elective Courses</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Minor Courses</Sidebar.Item>
                   </Sidebar.Collapse>
-                  <div className="flex flex-col space-y-1 mt-7">
-                    <div className="px-8 py-0.5 bg-slate-500"></div>
-                  </div>
+                  <div className="border-b border-black my-2"></div>
 
-                  <Sidebar.Collapse label="Student portal">
-                    <Sidebar.Item href="#">Score Report </Sidebar.Item>
-                    <Sidebar.Item href="#">Assignment Submission</Sidebar.Item>
-                    <Sidebar.Item href="#">Student Research</Sidebar.Item>
-                    <Sidebar.Item href="#">Phone Number</Sidebar.Item>
-                    <Sidebar.Item href="#">Line Chat Bot</Sidebar.Item>
+                  <Sidebar.Collapse className="hover:bg-[#e8e8e8] " label="Student portal">
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Score Report </Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Assignment Submission</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Student Research</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Phone Number</Sidebar.Item>
+                    <Sidebar.Item className="hover:bg-[#e8e8e8] " href="#">Line Chat Bot</Sidebar.Item>
                   </Sidebar.Collapse>
-                  <div className="flex flex-col space-y-1 mt-7">
-                    <div className="px-8 py-0.5 bg-slate-500"></div>
-                  </div>
+                  <div className="border-b border-black my-2"></div>
                 </Sidebar.ItemGroup>
               </Sidebar.Items>
             </Sidebar>
