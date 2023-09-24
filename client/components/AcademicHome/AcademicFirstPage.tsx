a81be4a76f881cdae92fb85df0e4614d8b4a4256
KEN <Thanaton_b@cmu.ac.th>
2023-09-24 19:27:20 +0700

    update

--- client/components/AcademicHome/AcademicFirstPage.tsx
+++ client/components/AcademicHome/AcademicFirstPage.tsx
@@ -12,21 +12,21 @@ const AcademicFirstPage = (props: Props) => {
       content:
         "The Computer Science Department admits students to an undergraduate program that you are guaranteed to find rigorous and dedicated to the real-world training and practical problem solving that has been the hallmark of computer science education at CMU since its inception.",
       href: "#",
-      image: "/bc.jpg",
+      image: "/1.jpg",
     },
     {
       title: "Master",
       content:
         "We offer three distinct master’s options for students who have completed (or will complete) a bachelor’s degree and want to extend their training in computer science. Our largest program is the M.S. in Computer Science, which allows students to work with their academic advisor to create their own course of study.",
       href: "#",
-      image: "/master.jpg",
+      image: "/2.jpg",
     },
     {
       title: "DOCTORAL",
       content:
         "Chiang Mai University's Ph.D. in Computer Science is, above all, a research degree. We certify that our students have a broad foundation and awareness of core concepts in computer science, have advanced the field by performing significant original research and have reported that work in a scholarly fashion.",
       href: "#",
-      image: "/doctor.jpg",
+      image: "/3.jpg",
     },
   ];
   return (
