16cd261ca03455fdd8c94eaa00fcbeb49e3bf898
KENTHN658 <118724637+KENTHN658@users.noreply.github.com>
2023-09-30 02:24:07 +0700

    updateLocalSwitcher

--- client/components/LocaleSwitcher.tsx
+++ client/components/LocaleSwitcher.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import {useLocale} from 'next-intl';
+import { useLocale } from "next-intl";
 
 export default function LocaleSwitcher() {
   const locales = useLocale();
@@ -14,15 +14,21 @@ export default function LocaleSwitcher() {
 
   const redirectedPathName = (locale: string) => {
     if (!pathName) return "/";
-    const segments = pathName.split("/");
-    segments[1] = locale;
+    let segments = pathName.split("/");
+
+    if (i18n.locales.includes(segments[1])) {
+      segments[1] = locale;
+    } else {
+      segments.splice(1, 0, locale);
+    }
+
     return segments.join("/");
   };
 
   return (
     <>
       <ul className="flex gap-x-3">
-        {i18n.locales.map((locale,index) => {
+        {i18n.locales.map((locale, index) => {
           return (
             <li key={locale}>
               <Link
@@ -33,7 +39,7 @@ export default function LocaleSwitcher() {
               </Link>
             </li>
           );
-        })}
+        })} 
       </ul>
     </>
   );
