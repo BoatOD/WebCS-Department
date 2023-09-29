67457aeda898dbdd13fba667867f684944eb57bc
chada2546 <chadaphorn_penprapai@cmu.ac.th>
2023-09-29 22:02:04 +0700

    about again

--- client/app/[locale]/(home)/about/history/page.tsx
+++ client/app/[locale]/(home)/about/history/page.tsx
@@ -7,7 +7,7 @@ import Sidebar1 from "@/components/Sidebar1";
 
 type Props = {};
 
-export default function FAQs({ }: Props) {
+export default function history({ }: Props) {
   return (
     <>
       <Banner
