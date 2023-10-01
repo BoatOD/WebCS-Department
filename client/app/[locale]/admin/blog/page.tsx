0b65290dc34042a971b23b4d5468638edb688512
kitsadi <kitsadi_than@cmu.ac.th>
2023-10-01 15:36:46 +0700

    Update page.tsx

--- client/app/[locale]/admin/blog/page.tsx
+++ client/app/[locale]/admin/blog/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import SidebarWithCta from "@/components/admin/SidebarWithCta";
 import Link from 'next/link'
 import Image from 'next/image';
 
